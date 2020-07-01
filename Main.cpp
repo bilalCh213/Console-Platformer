3346fdf80b600eb281db1902f3fae845b209e8cb
bilalCh213 <boyz213@live.com>
2020-07-01 13:55:42 +0500

    Added a note.

--- Main.cpp
+++ Main.cpp
@@ -7,20 +7,18 @@
 #include <conio.h>
 #include <time.h>
 
+//NEW ADDITION TO CODE IN ORDER TO REPLACE OBSOLETE FUNCTIONS
 #include <windows.h>
 void usleep(__int64 usec) 
 { 
     HANDLE timer; 
     LARGE_INTEGER ft; 
-
-    ft.QuadPart = -(10*usec); // Convert to 100 nanosecond interval, negative value indicates relative time
-
+    ft.QuadPart = -(10*usec);
     timer = CreateWaitableTimer(NULL, TRUE, NULL); 
     SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0); 
     WaitForSingleObject(timer, INFINITE); 
     CloseHandle(timer); 
 }
-
 void clrscr()
 {
   system("cls");
